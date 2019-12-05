6a8c630fb00a6960201dfd818ab8fe1154bba2b6
HealYouDown <jeremyregitz@gmail.com>
2019-12-05 22:51:57 +0100

    nicer parameter mode determination

--- Day 5/puzzle_1.py
+++ Day 5/puzzle_1.py
@@ -13,13 +13,9 @@ while True:
     # 1 = immediate mode
     # checks the instruction string and if there are not enough modes given
     # fill up with 0's
-    parameter_modes = []
-    count = 0
+    parameter_modes = [0, 0, 0]
     for count, char in enumerate(reversed(instruction[:-2])):
-        parameter_modes.append(int(char))
-    while count < 2:
-        parameter_modes.append(0)
-        count += 1
+        parameter_modes.insert(count, int(char))
 
     if optcode in [1, 2]:
         param1_value = data[index+1]
